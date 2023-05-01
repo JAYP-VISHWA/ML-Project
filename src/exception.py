66373b629e64b813c9eb5f1fbb466f2278561eac
Jay Pratap <jay47.w@gmail.com>
2023-05-01 11:44:13 +0530

    Debugged exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,13 @@
 import sys
+import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
-    return error_message
     )
+    return error_message
 
 
 class CostumException(Exception):
@@ -16,3 +17,12 @@ class CostumException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+
+if __name__=="__main__":
+    try:
+        a = 1/0
+    except Exception as ex:
+        logging.info("Devide by zero")
+        raise CostumException(ex, sys)
+        
